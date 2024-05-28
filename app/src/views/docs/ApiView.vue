b1b40aaf21da4449d6cf51d6b96ff9846e9c35ce
Howard Smith <hj_smith@live.com>
2024-05-28 23:03:19 +0100

    feat: API docs

--- app/src/views/docs/ApiView.vue
+++ app/src/views/docs/ApiView.vue
@@ -1,6 +1,32 @@
 <template>
-  <v-card variant="elevated" class="mx-6 mt-13">
-    <v-card-title class="font-weight-black">This is the API docs page</v-card-title>
-    <v-card-text class="text-body-1"> TODO </v-card-text>
-  </v-card>
+  <api-reference :configuration="ScalarConfig" />
 </template>
+
+<script setup lang="ts">
+import { ApiReference } from '@scalar/api-reference'
+import { ref } from 'vue'
+
+const ScalarConfig = ref({
+  isEditable: false,
+  hideModels: true,
+  spec: {
+    url: 'http://127.0.0.1:8000/api/v1/docs/openapi.yaml'
+  },
+  // Limit the clients shown for the time being, just to make the page look a bit cleaner.
+  hiddenClients: {
+    c: ['libcurl'],
+    clojure: ['clj_http'],
+    java: ['unirest'],
+    javascript: ['axios', 'jquery'],
+    node: ['axios', 'request', 'undici', 'unirest'],
+    objc: ['nsurlsession'],
+    ocaml: ['cohttp'],
+    php: ['curl', 'guzzle', 'http1', 'http2'],
+    python: ['python3'],
+    r: ['httr'],
+    ruby: ['native'],
+    shell: ['httpie'],
+    swift: ['nsurlsession']
+  }
+})
+</script>
