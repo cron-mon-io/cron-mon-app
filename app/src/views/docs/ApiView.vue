64a1b8779ec2439a0dd83090ede83cc931dc9525
Howard Smith <hj_smith@live.com>
2024-05-29 22:34:51 +0100

    feat: Hack to use correct API URL

--- app/src/views/docs/ApiView.vue
+++ app/src/views/docs/ApiView.vue
@@ -6,11 +6,16 @@
 import { ApiReference } from '@scalar/api-reference'
 import { ref } from 'vue'
 
+const openApiSpec = await (await fetch('http://127.0.0.1:8000/api/v1/docs/openapi.yaml')).text()
+
 const ScalarConfig = ref({
   isEditable: false,
   hideModels: true,
   spec: {
-    url: 'http://127.0.0.1:8000/api/v1/docs/openapi.yaml'
+    // This is a bit of an ugly workaround to allow Scalar to hit the API, since it's
+    // running on a different domain.
+    // TODO: Figure out how to handy this for local dev and in real deployments.
+    content: openApiSpec.replace('- url: /api/v1', '- url: http://127.0.0.1:8000/api/v1')
   },
   // Limit the clients shown for the time being, just to make the page look a bit cleaner.
   hiddenClients: {
