43236e95394da4ce8cd043f45cc1193371a2d661
Howard Smith <hj_smith@live.com>
2024-06-24 14:00:28 +0100

    chore: OpenAPI specs don't have a `servers` section anymore

--- app/src/views/docs/ApiView.vue
+++ app/src/views/docs/ApiView.vue
@@ -14,7 +14,7 @@ const ScalarConfig = ref({
     // This is a bit of an ugly workaround to allow Scalar to hit the API, since it's
     // running on a different domain.
     // TODO: Figure out how to handle this for local dev and in real deployments.
-    content: openApiSpec.replace('- url: /api/v1', '- url: http://127.0.0.1:8000/api/v1')
+    content: openApiSpec + 'servers:\n  - url: http://127.0.0.1:8000'
   },
   // Limit the clients shown for the time being, just to make the page look a bit cleaner.
   hiddenClients: {
