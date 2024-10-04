c48eecead7e4f75452eb51cba5f9f09ea9dc5e60
Howard Smith <hj_smith@live.com>
2024-10-04 20:03:48 +0100

    chore: Remove unnecessary coverage exclusions and reduce minimum to 90%

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -11,20 +11,15 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
         exclude: [
-          'src/components/icons/**', // Icons are just SVGs, no logic.
           'src/main.ts', // Main is just bootstrapping, no logic.
-          'src/router/**', // Router is just configuration, no logic.
           'src/utils/testing/**', // Testing utilities are just for testing.
-          'src/views/docs/**', // Docs are just content, no logic.
-          'src/views/HomeView.vue', // Home page is just content, no logic.
-          'src/views/NotFoundView.vue', // 404 is just a placeholder, no logic.
           ...coverageConfigDefaults.exclude
         ],
         thresholds: {
-          lines: 95,
-          functions: 95,
-          branches: 95,
-          statements: 95
+          lines: 90,
+          functions: 90,
+          branches: 90,
+          statements: 90
         }
       }
     }
