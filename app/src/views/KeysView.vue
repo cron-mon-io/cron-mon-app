f3d271427b609fb4c45953db9af7acd5603cb3aa
Howard Smith <hj_smith@live.com>
2024-11-02 22:25:11 +0000

    feat: Revoke API keys

--- app/src/views/KeysView.vue
+++ app/src/views/KeysView.vue
@@ -1,63 +1,73 @@
 <template>
-  <ApiAlert class="mx-4 mt-4" :error="syncError" :retryEnabled="true" @retried="getApiKeys" />
-  <v-card class="elevation-2 mx-6 mt-13">
-    <v-card-title class="font-weight-black"> API Keys</v-card-title>
-    <v-card-text>
-      <div class="text-body-1">
-        <p>In order to integrate with Cron Mon you'll need an API key.</p>
-        <p>
-          Your existing keys are listed below and you can create a new key via the
-          <i>Generate New Key</i> button below.
-        </p>
-      </div>
-      <v-btn
-        append-icon="mdi-plus"
-        color="primary"
-        class="my-4"
-        :disabled="syncError !== null"
-        @click="generateKey"
-      >
-        Generate New Key
-        <v-tooltip activator="parent" location="top">Click to generate a new API key</v-tooltip>
-      </v-btn>
-      <v-table height="300px" style="width: 100%" fixed-header>
-        <thead>
-          <tr>
-            <th class="text-left" style="width: 30%">API KEY</th>
-            <th class="text-left" style="width: 25%">CREATED</th>
-            <th class="text-left" style="width: 40%">LAST ACCESS</th>
-            <th class="text-left" style="width: 5%">&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody class="text-body-1">
-          <tr v-for="key in apiKeys" :key="key.api_key_id">
-            <td class="py-4">
-              <div class="text-h6">{{ key.name }}</div>
-              <v-chip
-                prepend-icon="mdi-eye-off-outline"
-                color="teal-accent-4"
-                density="compact"
-                label
-                >{{ key.masked }}</v-chip
-              >
-            </td>
-            <td class="py-4">{{ formatLastUsed(key.created) }}</td>
-            <td v-if="key.last_used" class="py-4">
-              {{ formatLastUsed(key.last_used.time) }} by
-              <router-link
-                :to="{ name: 'monitor', params: { id: key.last_used.monitor_id } }"
-                target="_blank"
-                rel="noreferrer noopener"
-                >{{ key.last_used.monitor_name }}</router-link
-              >
-            </td>
-            <td v-else class="py-4 text-disabled">Never used</td>
-            <td><v-btn icon="mdi-key-remove" @click="revokeKey(key)"></v-btn></td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-card-text>
-  </v-card>
+  <div>
+    <ApiAlert class="mx-4 mt-4" :error="syncError" :retryEnabled="true" @retried="getApiKeys" />
+    <ApiAlert class="mx-4 mt-4" :error="revokeError" @closed="revokeError = null" />
+    <v-card class="elevation-2 mx-6 mt-13">
+      <v-card-title class="font-weight-black"> API Keys</v-card-title>
+      <v-card-text>
+        <div class="text-body-1">
+          <p>In order to integrate with Cron Mon you'll need an API key.</p>
+          <p>
+            Your existing keys are listed below and you can create a new key via the
+            <i>Generate New Key</i> button below.
+          </p>
+        </div>
+        <v-btn
+          append-icon="mdi-plus"
+          color="primary"
+          class="my-4"
+          :disabled="syncError !== null"
+          @click="generateKey"
+        >
+          Generate New Key
+          <v-tooltip activator="parent" location="top">Click to generate a new API key</v-tooltip>
+        </v-btn>
+        <v-table height="300px" style="width: 100%" fixed-header>
+          <thead>
+            <tr>
+              <th class="text-left" style="width: 30%">API KEY</th>
+              <th class="text-left" style="width: 25%">CREATED</th>
+              <th class="text-left" style="width: 40%">LAST ACCESS</th>
+              <th class="text-left" style="width: 5%">&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody class="text-body-1">
+            <tr v-for="key in apiKeys" :key="key.api_key_id">
+              <td class="py-4">
+                <div class="text-h6">{{ key.name }}</div>
+                <v-chip
+                  prepend-icon="mdi-eye-off-outline"
+                  color="teal-accent-4"
+                  density="compact"
+                  label
+                  >{{ key.masked }}</v-chip
+                >
+              </td>
+              <td class="py-4">{{ formatLastUsed(key.created) }}</td>
+              <td v-if="key.last_used" class="py-4">
+                {{ formatLastUsed(key.last_used.time) }} by
+                <router-link
+                  :to="{ name: 'monitor', params: { id: key.last_used.monitor_id } }"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  >{{ key.last_used.monitor_name }}</router-link
+                >
+              </td>
+              <td v-else class="py-4 text-disabled">Never used</td>
+              <td><v-btn icon="mdi-key-remove" @click="openRevokeKeyDialog(key)"></v-btn></td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-card-text>
+    </v-card>
+    <ConfirmationDialog
+      :dialogActive="keyToRevoke !== null"
+      title="Revoke this API Key?"
+      icon="mdi-key-remove"
+      question="Are you sure you want to revoke this API key? It will not be usable anymore, and this cannot be undone."
+      @dialog-complete="revokeDialogComplete"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -67,6 +77,7 @@ import { formatDistanceToNow } from 'date-fns'
 import ApiAlert from '@/components/ApiAlert.vue'
 import type { ApiKeyRepoInterface } from '@/repos/api-key-repo'
 import type { ApiKey } from '@/types/api-key'
+import ConfirmationDialog from '@/components/ConfirmationDialog.vue'
 
 const FIVE_MINUTES_MS = 5 * 60 * 1000
 
@@ -82,14 +93,35 @@ onUnmounted(() => {
 
 const loading = ref(true)
 const syncError = ref<string | null>(null)
+const revokeError = ref<string | null>(null)
 const apiKeys = ref<ApiKey[]>([])
+const keyToRevoke = ref<ApiKey | null>(null)
 
 function generateKey() {
   alert('Not implemented yet!')
 }
 
-function revokeKey(_key: ApiKey) {
-  alert('Not implemented yet!')
+function openRevokeKeyDialog(key: ApiKey) {
+  keyToRevoke.value = key
+}
+
+function closeRevokeDialog() {
+  keyToRevoke.value = null
+}
+
+async function revokeDialogComplete(confirmed: boolean) {
+  const apiKeyRepo = await getApiKeyRepo()
+  if (confirmed) {
+    try {
+      await apiKeyRepo.revokeKey(keyToRevoke.value as ApiKey)
+      // If we've successfully revoked the key, we can remove it from the list.
+      apiKeys.value = apiKeys.value.filter((key) => key.api_key_id !== keyToRevoke.value?.api_key_id)
+    } catch (e: unknown) {
+      revokeError.value = (e as Error).message
+    }
+  }
+
+  closeRevokeDialog()
 }
 
 function formatLastUsed(lastUsed: string): string {
