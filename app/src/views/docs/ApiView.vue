648d243e14d2105fc4ed316cfa2150d507c9313b
Howard Smith <hj_smith@live.com>
2024-11-15 23:35:45 +0000

    feat: Simplify `ApiReference` usage

--- app/src/views/docs/ApiView.vue
+++ app/src/views/docs/ApiView.vue
@@ -1,21 +1,26 @@
 <template>
-  <div v-if="!scalarConfig.spec.content" class="d-flex justify-center mt-10">
-    <v-progress-circular indeterminate size="85" width="6"></v-progress-circular>
-  </div>
-  <div v-else>
+  <div>
     <ApiReference :configuration="scalarConfig" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
+// TODO: Figure out how to handle this for local dev and in real deployments.
+const API_URL = 'http://127.0.0.1:8000'
 const scalarConfig = ref({
   isEditable: false,
   hideModels: true,
   spec: {
-    content: ''
+    url: `${API_URL}/api/v1/docs/openapi.yaml`
   },
+  servers: [
+    {
+      url: API_URL,
+      description: 'Cron Mon API server'
+    }
+  ],
   // Limit the clients shown for the time being, just to make the page look a bit cleaner.
   hiddenClients: {
     c: ['libcurl'],
@@ -32,14 +37,5 @@ const scalarConfig = ref({
     shell: ['httpie'],
     swift: ['nsurlsession']
   }
-})
-
-onMounted(async () => {
-  const openApiSpec = await (await fetch('http://127.0.0.1:8000/api/v1/docs/openapi.yaml')).text()
-
-  // This is a bit of an ugly workaround to allow Scalar to hit the API, since it's
-  // running on a different domain.
-  // TODO: Figure out how to handle this for local dev and in real deployments.
-  scalarConfig.value.spec.content = openApiSpec + 'servers:\n  - url: http://127.0.0.1:8000'
 })
 </script>
