1e92913bf5e8d390d66e3501462f4fdb33d5ea35
Howard Smith <hj_smith@live.com>
2025-03-09 22:18:48 +0000

    test: Add tests for `AlertConfigRepository`

--- app/src/types/alert-config.ts
+++ app/src/types/alert-config.ts
@@ -1,17 +1,25 @@
-export type AlertConfig = {
-    alert_config_id: string
+export type AlertConfigSummary = {
+  name: string
+  active: boolean
+  on_late: boolean
+  on_error: boolean
+  type: SlackAlertType
+}
+
+export type AlertConfigIdentity = AlertConfigSummary & {
+  alert_config_id: string
+}
+
+export type AlertConfig = AlertConfigIdentity & {
+  monitors: Array<{
+    monitor_id: string
     name: string
-    active: boolean
-    on_late: boolean
-    on_error: boolean
-    monitors: Array<{
-        monitor_id: string
-        name: string
-    }>
-    type: Record<string, SlackAlertType>
+  }>
 }
 
 export type SlackAlertType = {
+  Slack: {
     token: string
     channel: string
+  }
 }
